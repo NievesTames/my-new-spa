91ee8a8d7e4fb5064bcf16da75195457936c1ecb
Nieves <nieves.tames@the-cocktail.com>
2021-09-22 17:24:54 +0200

    aumentar/disminuir

--- src/components/CarOptions.vue
+++ src/components/CarOptions.vue
@@ -2,6 +2,10 @@
   <h2>Coche - Options API</h2>
   <p>Marca: {{ brand }}</p>
   <p>Modelo: {{ model }}</p>
+  <p>Potencia: {{ power }}</p>
+
+  <button @click="upPower">Aumentar></button>
+  <button @click="downPower">Disminuir</button>
   </template>
 
 <script>
@@ -10,8 +14,17 @@
     return {
       brand: "Seat",
       model: "Leon",
-    }
-  }
+      power: 50,
+    };
+  },
+  methods: {
+    upPower() {
+      console.log("Aumentando Potencia");
+    },
+    downPower() {
+      console.log("Bajando Potencia");
+    },
+  },
 
 };
 </script>
