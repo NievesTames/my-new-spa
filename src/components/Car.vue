dab9710b1a5074ef9ee2f4ff2dfa05e50a348797
Nieves <nieves.tames@the-cocktail.com>
2021-09-22 12:55:20 +0200

    create new components

---
+++ src/components/Car.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Coche</h2>
+  <p>Marca: {{ brand }}</p>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            brand: "Audi",
+        };
+    },
+};
+
+/*Asi declaramos una varibale de opciones*/
+
+</script>
+
+<style>
+
+</style>
+
